529d324d7b601d10f2214bbc77292dd086ecf9d8
Vinita Boolchandani <vinitab@elogin1.bigred3.uits.iu.edu>
2021-03-03 19:40:45 -0500

    backend to add ratio

--- src/preprocessing/main.cpp
+++ src/preprocessing/main.cpp
@@ -19,9 +19,9 @@ int main(int argc, const char *argv[]) {
     /*User Inputs*/
     //string ligandNumText = "USERINPUT_LIGAND_NUMBER";
     //string npChargeText = "USERINPUT_NP_CHARGE";
-    //string ligandNumFileNameText = "USERINPUT_LIGAND_FILENAME";
     //string mpiText = "NODESIZE";
 
+    string ligandNumFileNameText = "USERINPUT_LIGAND_FILENAME";
     string saltText = "USERINPUTSALT";
     string dataSetCountText = "USERINPUT_DATASETCOUNT";
 
@@ -33,6 +33,7 @@ int main(int argc, const char *argv[]) {
     const double pi = 3.141593;
     // big for NP, small for ligand
     int n, Q_vlp1, Q_vlp2;       //dev input
+    int e2k2ratio; //user input
     double c;       //user input
     double D, d;    //dev input, but could be made user input
     int q;          //dev input
@@ -49,6 +50,7 @@ int main(int argc, const char *argv[]) {
             ("help,h", "print usage message")
             ("Qvlp1,E", value<int>(&Q_vlp1)->default_value(-1500), "Q_E2 in e")
             ("Qvlp2,K", value<int>(&Q_vlp2)->default_value(-600), "Q_K2 in e")
+            ("e2k2ratio,r", value<int>(&e2k2ratio)->default_value(1), "e2:k2 in multiples")
             ("NLigand,n", value<int>(&n)->default_value(100))
             ("Salt,c", value<double>(&c)->default_value(0.150), "c in Molars")
             ("qnp,q", value<int>(&q)->default_value(45), "q in e")
@@ -74,20 +76,25 @@ int main(int argc, const char *argv[]) {
 
     double devInputQvlp1Charge = Q_vlp1 * (exp((1.64399 * 56) / sqrt(1 / c)) / (1 + ((1.64399 * 56) / sqrt(1 / c))));
     devInputQvlp1Charge = devInputQvlp1Charge * (1.6018 * 1e-19) / (1.60074 * 1e-19);
-    // cout<<"\n line 77 Q_vlp2:"<<Q_vlp2<<"\n";
+    
     double devInputQvlp2Charge = Q_vlp2 * (exp((1.64399 * 56) / sqrt(1 / c)) / (1 + ((1.64399 * 56) / sqrt(1 / c))));
     devInputQvlp2Charge = devInputQvlp2Charge * (1.6018 * 1e-19) / (1.60074 * 1e-19);
-    // cout<<"\n line 80 devInputQvlp2Charge:"<<devInputQvlp2Charge<<"\n";   
+    
     double devInputLigandCharge = q * (exp((1.64399 * 6.7) / sqrt(1 / c)) / (1 + ((1.64399 * 6.7) / sqrt(1 / c))));
     devInputLigandCharge = devInputLigandCharge * (1.6018 * 1e-19) / (1.60074 * 1e-19);
 
     double userInputSalt = 1 / (1e3 * sqrt(1 / (2 * 4 * pi * 0.714295 * (6.022 * 1e23) * c)));
     //double userInputSalt = 3.2879795708993616*1e9/sqrt(1/c);
     userInputSalt = userInputSalt * 56 * 1e-9;
-    // cout<<"\n I am here"<<userInputSalt;
+    
+    
     std::string initcoords = "initCoords_" + std::to_string(n);
-    initcoords = initcoords + "x.assembly";
-
+    if(e2k2ratio==1)
+        initcoords = initcoords+"x_1-1";
+    else if(e2k2ratio == 4)
+        initcoords = initcoords+"x_1-4";
+    initcoords = initcoords + ".assembly";
+    cout<<"\n ===>"<<initcoords;
     ofstream inputScript("in.lammps", ios::trunc);
     if (inputScript.is_open()) {
 
@@ -125,9 +132,9 @@ int main(int argc, const char *argv[]) {
                 if (found != std::string::npos)
                     line.replace(found, dataSetCountText.length(), std::to_string(dataSetCount));
 
-//                found = line.find(ligandNumFileNameText);
-//                if (found != std::string::npos)
-//                    line.replace(found, ligandNumFileNameText.length(), initcoords);
+               found = line.find(ligandNumFileNameText);
+               if (found != std::string::npos)
+                   line.replace(found, ligandNumFileNameText.length(), initcoords);
 
                 inputScript << line << endl;
             }
